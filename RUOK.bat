536a3444eea344b80d4b502c3a14974a8fe5f20a
FIFCOM <1280874899@qq.com>
2017-04-11 12:56:26 +0800

    Update RUOK.bat

--- RUOK.bat
+++ RUOK.bat
@@ -9,7 +9,7 @@
 if "%1"=="h" goto 1
 start mshta vbscript:createobject("wscript.shell").run("""%~nx0"" h",0)(window.close)&&exit 
 :1
-start start mshta vbscript:msgbox("Are you OK？",0,"雷军：")(window.close)&&exit
+start mshta vbscript:msgbox("Are you OK？",0,"雷军：")(window.close)&&exit
 (window.close)&&exit
 ping -n 1 127.1>nul
 goto 2
